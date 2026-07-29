c5abc39b221b1b6954421a0829644544aa877630
Shreya Rastogi <shreya9236094193@gmail.com>
2026-07-29 10:01:47 +0530

    done

---
+++ Day1.js
@@ -0,0 +1,27 @@
+// console.log("hello world")
+// let a=30;
+// console.log(a)
+// //object
+// let s={
+//     name:"Shreya",
+//     age:20
+// };
+// console.log(s);
+// //constant
+// const p={
+//     name:"Shreya",
+//     age:20
+// };
+// console.log(p);
+// p.name="Archi"
+// console.log(p)
+//array
+const f=["Shreya","Archi"];
+f.push("Vrinda");
+f[0]="Rastogi";
+console.log(f);
+
+
+//Type of Error
+console.loglog("h" //syntax error
+    console.log(name);
