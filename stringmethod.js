bb29cf69ed3c46df8d6dfd591d4bdf0850afd8ec
Shreya Rastogi <shreya9236094193@gmail.com>
2026-07-29 10:17:26 +0530

    done

---
+++ stringmethod.js
@@ -0,0 +1,6 @@
+let text="javascript programming";
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.includes("hello"));
+console.log(text.substring());
