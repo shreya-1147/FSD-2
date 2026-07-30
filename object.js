e079f58722c4a9caec2245a9aeb2d5a8ba4f9a2e
Shreya Rastogi <shreya9236094193@gmail.com>
2026-07-30 11:51:31 +0530

    done

---
+++ object.js
@@ -0,0 +1,8 @@
+//mathobject
+console.log(Math.PI);
+console.log(Math.pow(5,3));
+console.log(Math.max(10,20,30,40));
+console.log(Math.min(10,20,30,40));
+console.log(Math.ceil(10.01));
+console.log(Math.round(10.01));
+console.log(Math.floor(Math.random()*100)+1);
